a57f45dd4583ba09a835d8198258473023ecbb57
张华厂 <zhanghuachang@yupaopao.cn>
2019-05-23 09:57:40 +0800

    公众号菜单管理

--- user-wechat-core/src/main/java/com/user/wechat/service/controller/UserController.java
+++ user-wechat-core/src/main/java/com/user/wechat/service/controller/UserController.java
@@ -5,6 +5,7 @@ import com.user.wechat.api.enums.ResponseCode;
 import com.user.wechat.api.request.UserRequest;
 import com.user.wechat.api.response.Response;
 import com.user.wechat.service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-04-12
  */
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -30,6 +32,7 @@ public class UserController {
     public Response getUserLogin(@RequestBody UserRequest userRequest) {
         UserDTO userDTO = userService.getUserByNameAndPwd(userRequest);
         if (ObjectUtils.isEmpty(userDTO)) {
+            log.error("登录失败,用户名或密码错误!");
             return Response.fail(ResponseCode.FAIL);
         }
         return Response.success(userDTO);
